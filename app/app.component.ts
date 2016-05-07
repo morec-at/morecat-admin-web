f9f6e344371ddea5fc6c07b6aa59d3bbaa6f92d0
emag <lanabe.lanabe@gmail.com>
2016-05-07 21:16:10 +0900

    Update Libraries version

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,10 @@
-import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {Component} from '@angular/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
 
 @Component({
   selector: 'app',
   template: `<h1>{{title}}</h1>`
 })
-
 export class AppComponent {
   title = 'morecat admin web';
 }
