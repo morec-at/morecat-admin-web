e78a5e967bc29a330ea524795588ff1bc1fdb7f3
emag <lanabe.lanabe@gmail.com>
2016-05-14 23:30:09 +0900

    Rough entries

--- app/components/entries.component.ts
+++ app/components/entries.component.ts
@@ -1,7 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CORE_DIRECTIVES} from '@angular/common';
+import {Router, RouterLink, RouteParams} from '@angular/router-deprecated';
+
+import {EntryService} from '../services/entry.service';
 
 @Component({
   selector: 'entries',
-  template: '<h1>Entries</h1>'
+  directives: [CORE_DIRECTIVES, RouterLink],
+  templateUrl: 'app/components/entries.component.html'
 })
-export class EntriesComponent {}
+export class EntriesComponent implements OnInit {
+
+  private entries:Object;
+  private loading:boolean;
+
+  constructor(private entryService:EntryService) {}
+
+  ngOnInit():void {
+    this.loading = true;
+    this.entryService
+      .getAll()
+      .subscribe((res:any) => {
+        this.entries = res.json().elements;
+        this.loading = false;
+      });
+  }
+
+}
